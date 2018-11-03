c6d2ebdbd4013818cb390f3084749ee321f91aed
Simon Sejer Nielsen <sinie45@gmail.com>
2018-11-03 21:58:31 +0100

    upgraded to Engine v. 4.19.3 Removed a couple of !ensure checks

--- BattleTank/BattleTanks/Source/BattleTanks/Private/TankAIController.cpp
+++ BattleTank/BattleTanks/Source/BattleTanks/Private/TankAIController.cpp
@@ -21,7 +21,7 @@ void ATankAIController::SetPawn(APawn * InPawn)
 	if (InPawn)
 	{
 		auto PossessedTank = Cast<ATank>(InPawn);
-		if (!ensure(PossessedTank)){return;}
+		if (PossessedTank){return;}
 
 		// Subscribe our local method to the tanks death event
 		PossessedTank->OnDeath.AddUniqueDynamic(this, &ATankAIController::OnPossedTankDeath);
@@ -43,7 +43,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn(); /// Casting this we make sure that it finds the player tank, and not just any player pawn
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)){ return; }
+	if (!PlayerTank && ControlledTank){ return; }
 	
 	// Move towards the player
 	MoveToActor(PlayerTank, AcceptanceRadius);
