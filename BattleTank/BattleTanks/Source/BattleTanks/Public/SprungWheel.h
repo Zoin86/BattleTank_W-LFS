45a688cad7ee9ef54b30929043bfe37440567e54
Simon Sejer Nielsen <sinie45@gmail.com>
2018-11-04 03:34:13 +0100

    Added C++ Blueprint version of the Spring Actor

---
+++ BattleTank/BattleTanks/Source/BattleTanks/Public/SprungWheel.h
@@ -0,0 +1,41 @@
+// Copyright Simon Sejer Nielsen
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "SprungWheel.generated.h"
+
+UCLASS()
+class BATTLETANKS_API ASprungWheel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASprungWheel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	// Components
+	UPROPERTY(VisibleAnywhere, Category = Component)
+	UStaticMeshComponent* Mass = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Component)
+	UStaticMeshComponent* Wheel = nullptr;
+
+	UPROPERTY(VisibleAnywhere, Category = Component)
+	UPhysicsConstraintComponent* PhysicsConstraint = nullptr;
+
+	
+	
+};
